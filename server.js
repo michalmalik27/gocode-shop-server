ce8c98a4fb8b62532a06cc99ca44fcd8f5912dea
michalmalik27 <michalush17@gmail.com>
2021-05-25 10:00:15 +0300

    גג

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const products_url = "/products";
 const products_file = "products.json";
 
+app.use(express.json);
+
 const app = express();
 
 app.get(products_url, (req, res) => {
@@ -29,6 +31,10 @@ app.get(`${products_url}/:id`, (req, res) => {
       res.send();
     }
   });
+});
+
+app.post(products_url, (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(8080);
