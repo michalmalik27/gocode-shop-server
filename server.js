74e65e4c95b0687cdf85a0dd3f48e8161fe84ee0
michalmalik27 <michalush17@gmail.com>
2021-05-31 13:21:02 +0300

    CRUD functions

--- server.js
+++ server.js
@@ -1,40 +1,126 @@
 const express = require("express");
 const fs = require("fs");
 
+const app = express();
+app.use(express.json());
+
 const products_url = "/products";
 const products_file = "products.json";
 
-app.use(express.json);
+const readProducts = (callback) => {
+  fs.readFile(products_file, "utf8", (err, products) => {
+    const productsArray = JSON.parse(products);
+    callback(productsArray);
+  });
+};
 
-const app = express();
+const writeProducts = (products, callback) => {
+  fs.writeFile(products_file, JSON.stringify(products), (err) => {
+    callback(err);
+  });
+};
 
 app.get(products_url, (req, res) => {
-  fs.readFile(products_file, "utf8", (err, products) => {
-    if (products) {
-      res.send(products);
-    } else {
-      res.status(500);
-      res.send();
-    }
-  });
+  const { search, category, minPrice, maxPrice } = req.query;
+  try {
+    readProducts((productsArray) => {
+      const result = productsArray.filter(
+        (p) =>
+          (!search ||
+            p.title.includes(search) ||
+            p.description.includes(search)) &&
+          (!category || p.category === category) &&
+          (!minPrice || p.price >= +minPrice) &&
+          (!maxPrice || p.price <= +maxPrice)
+      );
+      res.send(result);
+    });
+  } catch (error) {
+    res.status(500);
+    res.send();
+  }
 });
 
 app.get(`${products_url}/:id`, (req, res) => {
-  fs.readFile(products_file, "utf8", (err, products) => {
-    const product = JSON.parse(products).find(
-      (item) => item.id === +req.params.id
-    );
-    if (product) {
+  try {
+    readProducts((productsArray) => {
+      const product = productsArray.find((item) => item.id === +req.params.id);
       res.send(product);
-    } else {
-      res.status(404);
-      res.send();
+    });
+  } catch (error) {
+    res.status(500);
+    res.send();
+  }
+});
+
+app.post(`${products_url}`, (req, res) => {
+  try {
+    const { title, price, description, category, image } = req.body;
+    if (!title || !price || !description || !category || !image) {
+      res.status(500);
+      res.send("Invalid model");
+      return;
     }
-  });
+    readProducts((productsArray) => {
+      const id = Math.max(...productsArray.map((p) => p.id)) + 1;
+      const newProduct = { id, title, price, description, category, image };
+      writeProducts([...productsArray, newProduct], (err) => {
+        res.send(!!err ? "Failed" : "Success");
+      });
+    });
+  } catch (error) {
+    res.status(500);
+    res.send();
+  }
 });
 
-app.post(products_url, (req, res) => {
-  console.log(req.body);
+app.put(`${products_url}/:id`, (req, res) => {
+  try {
+    const { title, price, description, category, image } = req.body;
+    const { id } = req.params;
+
+    readProducts((productsArray) => {
+      const product = productsArray.find((p) => p.id === +id);
+      if (!product) {
+        res.status(500);
+        res.send("Product not exists");
+        return;
+      }
+
+      product.title = title || product.title;
+      product.price = price || product.price;
+      product.description = description || product.description;
+      product.category = category || product.category;
+      product.image = image || product.image;
+
+      const updatedProductsArray = productsArray.map((p) =>
+        p.id === +id ? product : p
+      );
+
+      writeProducts(updatedProductsArray, (err) => {
+        res.send(!!err ? "Failed" : "Success");
+      });
+    });
+  } catch (error) {
+    res.status(500);
+    res.send();
+  }
 });
 
-app.listen(8080);
+app.delete(`${products_url}/:id`, (req, res) => {
+  try {
+    readProducts((productsArray) => {
+      const products = productsArray.filter(
+        (item) => item.id !== +req.params.id
+      );
+      writeProducts(products, (err) => {
+        res.send(!!err ? "Failed" : "Success");
+      });
+    });
+  } catch (error) {
+    res.status(500);
+    res.send();
+  }
+});
+
+app.listen(8082);
